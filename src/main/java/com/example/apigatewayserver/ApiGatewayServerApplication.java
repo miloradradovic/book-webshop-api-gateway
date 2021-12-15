a3981dccd3705702bfedc8deb912648a83154949
miloradradovic <radovic.milorad1998@gmail.com>
2021-12-15 11:49:07 +0100

    fix

--- src/main/java/com/example/apigatewayserver/ApiGatewayServerApplication.java
+++ src/main/java/com/example/apigatewayserver/ApiGatewayServerApplication.java
@@ -1,17 +1,11 @@
 package com.example.apigatewayserver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
 public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
